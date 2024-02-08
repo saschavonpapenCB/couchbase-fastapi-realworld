f628b8701aa0c79e6b6e703375b7663303d20ef4
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-02-08 18:34:20 +1000

    Added request bodies for article path operation functions.

--- app/routers/article.py
+++ app/routers/article.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+from ..models.article import ArticleModel
 
 router = APIRouter(
     prefix="/articles",
@@ -7,49 +8,124 @@ router = APIRouter(
 )
 
 
-@router.get("/")
+@router.get(
+    "/",
+    response_model=list[ArticleModel],
+    description="Get a list of article. Optionally, you can filter by tag, author & favorited user. The list can also be limited and offset. \n\n Method: `list_articles`",
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def list_articles():
     #TBC
     return {"GET list of articles" : "Returns multiple Articles"}
 
 
-@router.get("/feed")
+@router.get(
+    "/feed",
+    response_model=list[ArticleModel],
+    description="Get a list of feed article. Ordered by most recent first. The list can also be limited and offset. \n\n Method: `list_feed_articles`",
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def list_feed_articles():
     #TBC
     return {"GET list of feed articles" : "Returns multiple Articles"}
 
 
-@router.get("/{slug}")
+@router.get(
+    "/{slug}",
+    response_model=ArticleModel,
+    description="Get a single article. \n\n Method: `get_article`",
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def get_article():
     #TBC
     return {"GET article" : "Returns Article"}
 
 
-@router.post("/")
+@router.post(
+    "/",
+    response_model=ArticleModel,
+    description="Create a single article. \n\n Method: `create_article`",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def create_article():
     #TBC
     return {"POST create article" : "Returns Article"}
 
 
-@router.put("/{slug}")
+@router.put(
+    "/{slug}",
+    response_model=ArticleModel,
+    description="Update a single article. \n\n Method: `update_article`",
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def update_article():
     #TBC
     return {"PUT update article" : "Returns Article"}
 
 
-@router.delete("/{slug}")
+@router.delete(
+    "/{slug}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Delete a single article. \n\n Method: `delete_article`",
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def delete_article():
     #TBC
     return {"DELETE article" : "Does not return"}
 
 
-@router.post("/{slug}/favorite")
+@router.post(
+    "/{slug}/favorite",
+    response_model=ArticleModel,
+    description="Favorite a single article. \n\n Method: `favorite_article`",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def favorite_article():
     #TBC
     return {"POST favorite article" : "Returns Article"}
 
 
-@router.post("/{slug}/favorite")
+@router.delete(
+    "/{slug}/favorite",
+    status_code=status.HTTP_200_OK,
+    response_model=ArticleModel,
+    description="Unfavorite a single article. \n\n Method: `unfavorite_article`",
+    responses={
+        500: {
+            "description": "Unexpected Error",
+        },
+    },
+)
 async def unfavorite_article():
     #TBC
-    return {"POST unfavorite article" : "Returns Article"}
+    return {"DELETE unfavorite article" : "Returns Article"}
