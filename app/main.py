a4a9f5b0e79e70c18b6e9c77dffa15d8b12bcf3a
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-02-09 21:29:21 +1000

    Hooked up Capella database lifespan.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 
 from .internal import admin
 from .routers import article, comment, profile, tag, user
+from .database import get_db
 
 
-app = FastAPI()
+# Initialize couchbase connection
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Method that gets called upon app initialization to initialize couchbase connection & close the connection on exit"""
+    db = get_db()
+    yield
+    db.close()
+
+
+app = FastAPI(
+    title="Couchbase FastAPI RealWorld",
+    version="1.0.0",
+    description="""
+    RealWorld backend built with FastAPI and Couchbase Capella.
+    """,
+    lifespan=lifespan,
+)
 
 
 app.include_router(article.router)
