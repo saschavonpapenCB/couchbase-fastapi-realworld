8a8525b9c94500b4651e57f188b3dd02a5723f06
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-02-10 13:17:16 +1000

    Implement path operator function response types and parameters.

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,5 +1,14 @@
-from fastapi import APIRouter
-from ..models.user import UserModel
+from fastapi import APIRouter, Body, Depends
+
+from ..schemas.user import LoginUser, NewUser, UpdateUser, User, UserResponse
+from ..utils.security import (
+    OAUTH2_SCHEME,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+    get_current_user_instance,
+    get_password_hash,
+)
 
 
 router = APIRouter(
@@ -10,7 +19,7 @@ router = APIRouter(
 
 @router.post(
     "/users/login",
-    response_model=UserModel,
+    response_model=UserResponse,
     description="Authenticate user. \n\n Method: `user_auth`",
     responses={
         500: {
@@ -18,14 +27,16 @@ router = APIRouter(
         },
     },
 )
-async def user_auth() -> UserModel:
+async def user_auth(
+    user_input: LoginUser = Body(..., embed=True, alias="user"),
+):
     # Need to implement response return
     return {"POST user authentication" : "Returns a User"}
 
 
 @router.post(
     "/users/",
-    response_model=UserModel,
+    response_model=UserResponse,
     description="Register user. \n\n Method: `user_reg`",
     responses={
         500: {
@@ -33,14 +44,16 @@ async def user_auth() -> UserModel:
         },
     },
 )
-async def user_reg() -> UserModel:
+async def user_reg(
+    user: NewUser = Body(..., embed=True),
+):
     # Need to implement response return
     return {"POST user registration" : "Returns a User"}
 
 
 @router.get(
     "/user",
-    response_model=UserModel,
+    response_model=UserResponse,
     description="Get the current user. \n\n Method: `current_user`",
     responses={
         500: {
@@ -48,14 +61,16 @@ async def user_reg() -> UserModel:
         },
     },
 )
-async def current_user() -> UserModel:
+async def current_user(
+    current_user: User = Depends(get_current_user),
+):
     # Need to implement response return
     return {"GET current user " : "Returns a User that is the current user"}
 
 
 @router.put(
     "/user",
-    response_model=UserModel,
+    response_model=UserResponse,
     description="Update the current user. \n\n Method: `update_user`",
     responses={
         500: {
@@ -63,6 +78,10 @@ async def current_user() -> UserModel:
         },
     },
 )
-async def update_user() -> UserModel:
+async def update_user(
+    update_user: UpdateUser = Body(..., embed=True, alias="user"),
+    user_instance: User = Depends(get_current_user_instance),
+    token: str = Depends(OAUTH2_SCHEME),
+):
     # Need to implement response return
     return {"PUT update user" : "Returns a User"}
