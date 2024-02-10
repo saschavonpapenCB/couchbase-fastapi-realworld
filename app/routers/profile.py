b84cbd05d6d882dc99ffd42ac40eed4ff5cf9b56
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-02-10 12:45:24 +1000

    Path operator function parameters implemented and response_model fixed.

--- app/routers/profile.py
+++ app/routers/profile.py
@@ -1,5 +1,9 @@
-from fastapi import APIRouter, status
-from ..models.user import ProfileModel
+from fastapi import APIRouter, status, Depends
+
+from ..models.user import UserModel
+from ..schemas.user import Profile, ProfileResponse
+from ..utils.security import get_current_user_instance, get_current_user_optional_instance
+
 
 router = APIRouter(
     prefix="/profiles",
@@ -10,7 +14,7 @@ router = APIRouter(
 
 @router.get(
     "/{username}",
-    response_model=ProfileModel,
+    response_model=ProfileResponse,
     description="Get a single profile. \n\n Method: `get_profile`",
     responses={
         500: {
@@ -18,14 +22,17 @@ router = APIRouter(
         },
     },
 )
-async def get_profile() -> ProfileModel:
+async def get_profile(
+    username: str,
+    logged_user: UserModel | None = Depends(get_current_user_optional_instance),
+):
     # Need to implement response return
     return {"GET profile" : "Returns a Profile"}
 
 
 @router.post(
     "/{username}/follow",
-    response_model=ProfileModel,
+    response_model=ProfileResponse,
     description="Follow a single profile. \n\n Method: `follow_profile`",
     status_code=status.HTTP_201_CREATED,
     responses={
@@ -34,14 +41,17 @@ async def get_profile() -> ProfileModel:
         },
     },
 )
-async def follow_profile() -> ProfileModel:
+async def follow_profile(
+    username: str,
+    user_instance: UserModel = Depends(get_current_user_instance),
+):
     # Need to implement response return
     return {"POST follow profile" : "Returns a Profile"}
 
 
 @router.delete(
     "/{username}/follow",
-    response_model=ProfileModel,
+    response_model=ProfileResponse,
     description="Unfollow a single profile. \n\n Method: `unfollow_profile`",
     status_code=status.HTTP_200_OK,
     responses={
@@ -50,6 +60,9 @@ async def follow_profile() -> ProfileModel:
         },
     },
 )
-async def unfollow_profile() -> ProfileModel:
+async def unfollow_profile(
+    username: str,
+    user_instance: UserModel = Depends(get_current_user_instance),
+):
     # Need to implement response return
     return {"DELETE unfollow profile" : "Returns a Profile"}
