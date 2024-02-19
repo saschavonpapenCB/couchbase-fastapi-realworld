0939738d17b6aa355f03245d0a4f8a1b7c14b7d3
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-02-19 15:23:19 +1000

    Finished get_article_comments operator function.

---
+++ api/core/user.py
@@ -0,0 +1,32 @@
+from fastapi import HTTPException
+
+from .exceptions import UserNotFoundException
+from ..models.user import UserModel
+
+
+async def query_users_by_id(
+        id: str,
+        db
+) -> UserModel:
+    query = """
+        SELECT client.id,
+                client.email,
+                client.username,
+                client.bio,
+                client.image,
+                client.hashed_password
+            FROM client as client 
+            WHERE client.id=$id;
+    """
+    try:
+        queryResult = db.query(query, id=id)
+        response_data = [r for r in queryResult]
+        if not response_data:
+            raise UserNotFoundException()
+        else:
+            response_user = response_data[0]
+            return UserModel(**response_user)
+    except TimeoutError:
+        raise HTTPException(status_code=408, detail="Request timeout")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {e}")
--- api/routers/comment.py
+++ api/routers/comment.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Body, Depends, HTTPException
 from fastapi.encoders import jsonable_encoder
 
 from ..core.article import query_articles_by_slug
+from ..core.user import query_users_by_id
 from ..database import get_db
 from ..models.article import CommentModel
 from ..models.user import UserModel
@@ -51,3 +52,24 @@ async def add_article_comment(
     response_profile = ProfileResponseSchema(**user_instance.model_dump())
     response_comment = CommentSchema(author=response_profile, **comment_instance.model_dump())
     return SingleCommentResponseSchema(comment=response_comment)
+
+
+@router.get(
+    "/articles/{slug}/comments",
+    response_model=MultipleCommentsResponseSchema
+)
+async def get_article_comments(
+    slug: str,
+    db=Depends(get_db)
+):
+    article = await query_articles_by_slug(slug, db)
+    comments = [CommentModel(**comment)for comment in article.comments]
+    comment_authorIds = [comment.authorId for comment in comments]
+    comment_authors=[]
+    for author_id in comment_authorIds:
+        comment_authors.append(await query_users_by_id(author_id, db))
+    data = list(zip(range(len(comments)), range(len(comment_authors))))
+    return MultipleCommentsResponseSchema.from_comments_and_authors(data)
+
+
+
