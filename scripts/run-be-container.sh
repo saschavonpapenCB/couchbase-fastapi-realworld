392168cc890940f2026a38dace4c2cd4494ba73b
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-04-29 13:46:20 +1000

    container composition

--- scripts/run-be-container.sh
+++ scripts/run-be-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --env-file api/.env -p 8000:8000 realworld-backend
+docker run --env-file api/.env -p 8000:8000 realworld-backend
--- scripts/run-fe-container.sh
+++ scripts/run-fe-container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -p 8080:4200 realworld-frontend
+docker run -it -p 80:80 realworld-frontend
