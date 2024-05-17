015701dc4eb2ceec93d6084205ce132ff680d211
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-05-17 22:10:03 +1000

    debugging deploy dev

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 # tfsec:ignore:aws-ecr-repository-customer-key
 resource "aws_ecr_repository" "repository" {
   name                 = var.repository_name
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
   }
