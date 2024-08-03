bf34d82a2352a202e7eb639ea9860ff372ad9209
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-08-03 17:57:42 +1000

    pre definitions push

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,6 +1,115 @@
 
 data "aws_caller_identity" "current" {}
 
+resource "aws_ecs_service" "cypress" {
+  name            = "cypress-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.cypress.arn
+  desired_count   = var.cypress_desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = var.security_groups
+  }
+}
+
+resource "aws_ecs_service" "backend" {
+  name            = "backend-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.backend.arn
+  desired_count   = var.backend_desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = var.security_groups
+  }
+}
+
+resource "aws_ecs_service" "frontend" {
+  name            = "frontend-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.frontend.arn
+  desired_count   = var.frontend_desired_count
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = var.subnets
+    security_groups = var.security_groups
+  }
+}
+
+resource "aws_ecs_task_definition" "backend" {
+  family                = "backend-task"
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                   = var.backend_task_cpu
+  memory                = var.backend_task_memory
+
+  container_definitions = jsonencode([
+    {
+      name      = "backend-container"
+      image     = "${aws_ecr_repository.backend.repository_url}:${var.backend_image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.backend_container_port
+          hostPort      = var.backend_host_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "frontend" {
+  family                = "frontend-task"
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                   = var.frontend_task_cpu
+  memory                = var.frontend_task_memory
+
+  container_definitions = jsonencode([
+    {
+      name      = "frontend-container"
+      image     = "${aws_ecr_repository.frontend.repository_url}:${var.frontend_image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.frontend_container_port
+          hostPort      = var.frontend_host_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_task_definition" "cypress" {
+  family                = "cypress-task"
+  network_mode          = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                   = var.cypress_task_cpu
+  memory                = var.cypress_task_memory
+
+  container_definitions = jsonencode([
+    {
+      name      = "cypress-container"
+      image     = "${aws_ecr_repository.cypress.repository_url}:${var.cypress_image_tag}"
+      essential = true
+      portMappings = [
+        {
+          containerPort = var.cypress_container_port
+          hostPort      = var.cypress_host_port
+        }
+      ]
+    }
+  ])
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = var.ecs_cluster_name
+}
+
 # tfsec:ignore:aws-ecr-repository-customer-key
 resource "aws_ecr_repository" "backend" {
   name                 = var.backend_repository_name
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,3 +1,106 @@
+variable "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  type        = string
+  
+}
+
+variable "cypress_task_cpu" {
+  description = "The CPU units to allocate for the testing task"
+  type        = string
+}
+
+variable "cypress_task_memory" {
+  description = "The memory in MiB to allocate for the testing task"
+}
+
+variable "cypress_container_port" {
+  description = "The container port for the testing service"
+  type        = number
+}
+
+variable "cypress_host_port" {
+  description = "The host port for the testing service"
+  type        = number
+}
+
+variable "backend_task_cpu" {
+  description = "The CPU units to allocate for the backend task"
+  type        = string
+}
+
+variable "backend_task_memory" {
+  description = "The memory in MiB to allocate for the backend task"
+  type        = string
+}
+
+variable "backend_container_port" {
+  description = "The container port for the backend service"
+  type        = number
+}
+
+variable "backend_host_port" {
+  description = "The host port for the backend service"
+  type        = number
+}
+
+variable "frontend_task_cpu" {
+  description = "The CPU units to allocate for the frontend task"
+  type        = string
+}
+
+variable "frontend_task_memory" {
+  description = "The memory in MiB to allocate for the frontend task"
+}
+
+variable "frontend_container_port" {
+  description = "The container port for the frontend service"
+  type        = number
+}
+
+variable "frontend_host_port" {
+  description = "The host port for the frontend service"
+  type        = number
+}
+
+variable "cypress_desired_count" {
+  description = "The desired count of testing tasks"
+  type        = number
+}
+
+variable "backend_desired_count" {
+  description = "The desired count of backend tasks"
+  type        = number
+}
+
+variable "frontend_desired_count" {
+  description = "The desired count of frontend tasks"
+  type        = number
+}
+
+variable "subnets" {
+  description = "The subnets to deploy the ECS services into"
+  type        = list(string)
+}
+
+variable "security_groups" {
+  description = "The security groups to assign to the ECS services"
+  type        = list(string)
+}
+
+variable "cypress_image_tag" {
+  description = "The tag of the testing image to deploy"
+}
+
+variable "backend_image_tag" {
+  description = "The tag of the backend image to deploy"
+}
+
+variable "frontend_image_tag" {
+  description = "The tag of the frontend image to deploy"
+}
+
+
+
 variable "backend_repository_name" {
   description = "Name of the backend repo"
   type        = string
@@ -9,7 +112,7 @@ variable "frontend_repository_name" {
 }
 
 variable "cypress_repository_name" {
-  description = "Name of the cypress repo"
+  description = "Name of the testing repo"
   type        = string
 }
 
