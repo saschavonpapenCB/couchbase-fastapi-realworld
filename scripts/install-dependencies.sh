e3d6efe83578688d781d3d3b538d50fa51679197
saschavonpapenCB <sascha.vonpapen@couchbase.com>
2024-08-27 13:51:10 +1000

    new scripting layout

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-REQUIREMENTS_FILE="./api/requirements.txt"
-
-if [ ! -f "$REQUIREMENTS_FILE" ]; then
-    echo "Error: $REQUIREMENTS_FILE not found."
-    exit 1
-fi
-
-echo "Installing dependencies from $REQUIREMENTS_FILE..."
-pip install -r "$REQUIREMENTS_FILE"
-
-echo "Dependency installation completed."
+pip install -r "./api/requirements.txt"
